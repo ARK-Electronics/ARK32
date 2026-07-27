am32-firmware/AM32

ARK_G491_12S_CAN add target
Update firmware definitions and add RGB LED control functions for G431

Add CAN termination pin definitions and setup function for GPIO control

Make FDCAN1 TX/RX pins configurable for ARK CAN ESC

The ARK CAN ESC routes FDCAN1_TX to PB9 instead of PA12 used by all
other G431 DroneCAN targets. Add CAN_TX_PIN/PORT and CAN_RX_PIN/PORT
defines with defaults matching existing PA11/PA12 behavior, and set
the ARK target to use PB9 for TX.

Make ADC GPIO ports configurable for ARK CAN ESC

The ARK CAN ESC has voltage on PB11 (GPIOB) and current on PC3 (GPIOC),
unlike other G431 targets which use GPIOA for both. Add VOLTAGE_ADC_PORT
and CURRENT_ADC_PORT defines with GPIOA defaults, and use them in
ADC_Init() instead of the hardcoded GPIOA.

Enable current sensing for ARK CAN ESC
Add USE_CURRENT_SENSE to the target definition so the ADC scan sequence
includes the current channel. Fix the ADC sequencer length to 3 ranks
when USE_CURRENT_SENSE is defined, and set DMA transfer length to match.
Without this, only temperature and voltage were scanned, and current
telemetry would report garbage.

Switch ARK CAN ESC input capture to TIM16_CH1 on PB4

The ARK CAN ESC routes MTR_INPUT to PB4, which cannot be TIM15_CH1
(only PA2 supports that). PB4 AF1 = TIM16_CH1, so switch the G4_E
hardware group to USE_TIMER_16_CHANNEL_1.

Since TIM16 was the commutation timer (COM_TIMER) for all G431
targets, reassign COM_TIMER to TIM7 for the ARK target using the
existing #ifndef override pattern. Add MX_TIM7_Init() and
TIM7_IRQHandler for commutation, and wrap the existing TIM15/TIM16
init paths with appropriate #ifdefs.

Existing G431 targets are unaffected — they continue using TIM15 for
input capture and TIM16 as COM_TIMER.

Add CURRENT_OFFSET definition for improved current sensing accuracy

Refactor timer reset logic for Dshot DMA functions to improve clarity and maintainability

Add EEPROM_START_ADD definition for EEPROM memory management

Add RAMP_SPEED definit

Inc/targets.h
@@ -359,6 +359,56 @@
 #define USE_SERIAL_TELEMETRY
 #endif
 
+#ifdef ARK_G431_CAN
+#define FIRMWARE_NAME "ARK_G431_CAN"
+#define FILE_NAME "ARK_G431_CAN"
+#define DRONECAN_SUPPORT 1
+#define DRONECAN_NODE_NAME "com.ark_12s.esc"
+#define DEAD_TIME 65
+
+#define HARDWARE_GROUP_G4_E
+#define TARGET_STALL_PROTECTION_INTERVAL 20000
+#define USE_SERIAL_TELEMETRY
+#define VOLTAGE_ADC_PIN LL_GPIO_PIN_11
+#define VOLTAGE_ADC_CHANNEL LL_ADC_CHANNEL_14
+#define CURRENT_ADC_PIN LL_GPIO_PIN_3
+#define CURRENT_ADC_PORT GPIOC
+#define CURRENT_ADC_CHANNEL LL_ADC_CHANNEL_9
+#define VOLTAGE_ADC_PORT GPIOB
+#define USE_CURRENT_SENSE
+#define MILLIVOLT_PER_AMP 10
+#define CURRENT_OFFSET 1 // millivolts
+#define TARGET_VOLTAGE_DIVIDER 310
+#define RAMP_SPEED_LOW_RPM 1
+#define RAMP_SPEED_HIGH_RPM 1
+// #define NO_POLLING_START
+#define USE_RGB_LED
+#define RED_PORT    GPIOC
+#define RED_PIN     LL_GPIO_PIN_6
+#define GREEN_PORT  GPIOC
+#define GREEN_PIN   LL_GPIO_PIN_7
+#define BLUE_PORT   GPIOC
+#define BLUE_PIN    LL_GPIO_PIN_8
+
+#define CAN_TERM_PIN GPIO_PORT_PIN(2, 12) // PC12
+#define CAN_TERM_POLARITY 1 // active high
+
+#define EEPROM_START_ADD (uint32_t)0x0801F800
+
+#define CAN_TX_PIN LL_GPIO_PIN_9
+#define CAN_TX_PORT GPIOB
+#define CAN_RX_PIN LL_GPIO_PIN_11
+#define CAN_RX_PORT GPIOA
+
+#define COM_TIMER TIM7
+#define COM_TIMER_IRQ TIM7_IRQn
+
+#define USE_HSE
+#undef HSE_VALUE
+#define HSE_VALUE 8000000
+#define USE_HSE_BYPASS 0
+#endif
+
 #ifdef AS_G431
 #define FIRMWARE_NAME "AS G431"
 #define FILE_NAME "AS_G431"
@@ -3161,6 +3211,10 @@
 #define RAMP_SPEED_HIGH_RPM 16
 #endif
 
+#ifndef RAMP_DIVIDER
+#define RAMP_DIVIDER 0
+#endif
+
 /************************************ F051 Hardware Groups
  * ****************************/
 
@@ -4151,6 +4205,56 @@
 
 #define INPUT_DMA_CHANNEL LL_DMA_CHANNEL_1
 #define DMA_HANDLE_TYPE_DEF hdma_tim15_ch1
+#define IC_DMA_IRQ_NAME DMA1_Channel1_IRQn
+
+#define PHASE_A_GPIO_LOW LL_GPIO_PIN_15
+#define PHASE_A_GPIO_PORT_LOW GPIOB
+#define AF_A_LOW LL_GPIO_AF_4
+#define PHASE_A_GPIO_HIGH LL_GPIO_PIN_10
+#define PHASE_A_GPIO_PORT_HIGH GPIOA
+
+#define PHASE_B_GPIO_LOW LL_GPIO_PIN_14
+#define PHASE_B_GPIO_PORT_LOW GPIOB
+#define PHASE_B_GPIO_HIGH LL_GPIO_PIN_9
+#define PHASE_B_GPIO_PORT_HIGH GPIOA
+
+#define PHASE_C_GPIO_LOW LL_GPIO_PIN_13
+#define PHASE_C_GPIO_PORT_LOW GPIOB
+#define PHASE_C_GPIO_HIGH LL_GPIO_PIN_8
+#define PHASE_C_GPIO_PORT_HIGH GPIOA
+
+#define PHASE_C_COMP LL_COMP_INPUT_MINUS_IO2 // pa0
+#define PHASE_B_COMP LL_COMP_INPUT_MINUS_IO1 // pa4
+#define PHASE_A_COMP LL_COMP_INPUT_MINUS_IO1 // pa5
+
+#define PHASE_C_INPUT_PLUS  LL_COMP_INPUT_PLUS_IO1  //pa1
+#define PHASE_B_INPUT_PLUS  LL_COMP_INPUT_PLUS_IO1  //pa1
+#define PHASE_A_INPUT_PLUS  LL_COMP_INPUT_PLUS_IO2  //pa3
+
+#define PHASE_C_EXTI_LINE LL_EXTI_LINE_21
+#define PHASE_C_COMP_NUMBER COMP1
+
+#define PHASE_B_EXTI_LINE LL_EXTI_LINE_21
+#define PHASE_B_COMP_NUMBER COMP1
+
+#define PHASE_A_EXTI_LINE LL_EXTI_LINE_22
+#define PHASE_A_COMP_NUMBER COMP2
+
+
+#endif
+
+#ifdef HARDWARE_GROUP_G4_E
+
+#define MCU_G431
+#define USE_TIMER_16_CHANNEL_1
+#define INPUT_PIN LL_GPIO_PIN_4
+#define INPUT_PIN_PORT GPIOB
+#define IC_TIMER_CHANNEL LL_TIM_CHANNEL_CH1
+#define IC_TIMER_REGISTER TIM16
+#define IC_TIMER_POINTER htim16
+
+#define INPUT_DMA_CHANNEL LL_DMA_CHANNEL_1
+#define DMA_HANDLE_TYPE_DEF hdma_tim16_ch1
 #define IC_DMA_IRQ_NAME DMA1_Channel1_IRQn
 
 #define PHASE_A_GPIO_LOW LL_GPIO_PIN_15
@@ -5187,7 +5291,9 @@
 #define INTERVAL_TIMER TIM2
 #define TEN_KHZ_TIMER TIM6
 #define UTILITY_TIMER TIM17
+#ifndef COM_TIMER
 #define COM_TIMER TIM16
+#endif
 #define APPLICATION_ADDRESS 0x08001000
 #define MAIN_COMP COMP2
 #define EXTI_LINE LL_EXTI_LINE_22
@@ -5209,9 +5315,25 @@
 #ifndef AF_C_LOW
   #define AF_C_LOW LL_GPIO_AF_6
 #endif
- 
+#ifndef CAN_TX_PIN
+  #define CAN_TX_PIN LL_GPIO_PIN_12
+  #define CAN_TX_PORT GPIOA
+#endif
+#ifndef CAN_RX_PIN
+  #define CAN_RX_PIN LL_GPIO_PIN_11
+  #define CAN_RX_PORT GPIOA
+#endif
+#ifndef VOLTAGE_ADC_PORT
+  #define VOLTAGE_ADC_PORT GPIOA
+#endif
+#ifndef CURRENT_ADC_PORT
+  #define CURRENT_ADC_PORT GPIOA
+#endif
+
 #define DSHOT_PRIORITY_THRESHOLD 60
+#ifndef COM_TIMER_IRQ
 #define COM_TIMER_IRQ TIM1_UP_TIM16_IRQn
+#endif
 #endif
 
 #ifdef MCU_GDE23

ARK-Electronics/ARK32

Inc/targets.h
@@ -53,6 +53,57 @@
 #	define USE_SERIAL_TELEMETRY
 #endif
 
+#ifdef ARK_G431_CAN
+#	define FIRMWARE_NAME "ARK_G431_CAN"
+#	define FILE_NAME "ARK_G431_CAN"
+#	define DRONECAN_SUPPORT 1
+#	define DRONECAN_NODE_NAME "com.ark_12s.esc"
+#	define DEAD_TIME 65
+
+#	define HARDWARE_GROUP_G4_E
+#	define TARGET_STALL_PROTECTION_INTERVAL 20000
+#	define USE_SERIAL_TELEMETRY
+#	define VOLTAGE_ADC_PIN LL_GPIO_PIN_11
+#	define VOLTAGE_ADC_CHANNEL LL_ADC_CHANNEL_14
+#	define CURRENT_ADC_PIN LL_GPIO_PIN_3
+#	define CURRENT_ADC_PORT GPIOC
+#	define CURRENT_ADC_CHANNEL LL_ADC_CHANNEL_9
+#	define VOLTAGE_ADC_PORT GPIOB
+#	define USE_CURRENT_SENSE
+#	define MILLIVOLT_PER_AMP 10
+#	define CURRENT_OFFSET 1 // millivolts
+#	define TARGET_VOLTAGE_DIVIDER 310
+#	define RAMP_SPEED_LOW_RPM 1
+#	define RAMP_SPEED_HIGH_RPM 1
+// #define NO_POLLING_START
+#	define USE_RGB_LED
+#	define RED_PORT    GPIOC
+#	define RED_PIN     LL_GPIO_PIN_6
+#	define GREEN_PORT  GPIOC
+#	define GREEN_PIN   LL_GPIO_PIN_7
+#	define BLUE_PORT   GPIOC
+#	define BLUE_PIN    LL_GPIO_PIN_8
+
+#	define CAN_TERM_PIN GPIO_PORT_PIN(2, 12) // PC12
+#	define CAN_TERM_POLARITY 1 // active high
+
+#	define EEPROM_START_ADD (uint32_t)0x0801F800
+
+#	define CAN_TX_PIN LL_GPIO_PIN_9
+#	define CAN_TX_PORT GPIOB
+#	define CAN_RX_PIN LL_GPIO_PIN_11
+#	define CAN_RX_PORT GPIOA
+
+#	define COM_TIMER TIM7
+#	define COM_TIMER_IRQ TIM7_IRQn
+
+#	define USE_HSE
+#	undef HSE_VALUE
+#	define HSE_VALUE 8000000
+#	define USE_HSE_BYPASS 0
+#endif
+
+
 #ifdef AM32REF_F051
 // #define LOOP_FREQUENCY_HZ 10000
 #	define FILE_NAME "AM32REF_F051"
@@ -1203,6 +1254,56 @@
 
 #endif
 
+#ifdef HARDWARE_GROUP_G4_E
+
+#	define MCU_G431
+#	define USE_TIMER_16_CHANNEL_1
+#	define INPUT_PIN LL_GPIO_PIN_4
+#	define INPUT_PIN_PORT GPIOB
+#	define IC_TIMER_CHANNEL LL_TIM_CHANNEL_CH1
+#	define IC_TIMER_REGISTER TIM16
+#	define IC_TIMER_POINTER htim16
+
+#	define INPUT_DMA_CHANNEL LL_DMA_CHANNEL_1
+#	define DMA_HANDLE_TYPE_DEF hdma_tim16_ch1
+#	define IC_DMA_IRQ_NAME DMA1_Channel1_IRQn
+
+#	define PHASE_A_GPIO_LOW LL_GPIO_PIN_15
+#	define PHASE_A_GPIO_PORT_LOW GPIOB
+#	define AF_A_LOW LL_GPIO_AF_4
+#	define PHASE_A_GPIO_HIGH LL_GPIO_PIN_10
+#	define PHASE_A_GPIO_PORT_HIGH GPIOA
+
+#	define PHASE_B_GPIO_LOW LL_GPIO_PIN_14
+#	define PHASE_B_GPIO_PORT_LOW GPIOB
+#	define PHASE_B_GPIO_HIGH LL_GPIO_PIN_9
+#	define PHASE_B_GPIO_PORT_HIGH GPIOA
+
+#	define PHASE_C_GPIO_LOW LL_GPIO_PIN_13
+#	define PHASE_C_GPIO_PORT_LOW GPIOB
+#	define PHASE_C_GPIO_HIGH LL_GPIO_PIN_8
+#	define PHASE_C_GPIO_PORT_HIGH GPIOA
+
+#	define PHASE_C_COMP LL_COMP_INPUT_MINUS_IO2 // pa0
+#	define PHASE_B_COMP LL_COMP_INPUT_MINUS_IO1 // pa4
+#	define PHASE_A_COMP LL_COMP_INPUT_MINUS_IO1 // pa5
+
+#	define PHASE_C_INPUT_PLUS  LL_COMP_INPUT_PLUS_IO1  //pa1
+#	define PHASE_B_INPUT_PLUS  LL_COMP_INPUT_PLUS_IO1  //pa1
+#	define PHASE_A_INPUT_PLUS  LL_COMP_INPUT_PLUS_IO2  //pa3
+
+#	define PHASE_C_EXTI_LINE LL_EXTI_LINE_21
+#	define PHASE_C_COMP_NUMBER COMP1
+
+#	define PHASE_B_EXTI_LINE LL_EXTI_LINE_21
+#	define PHASE_B_COMP_NUMBER COMP1
+
+#	define PHASE_A_EXTI_LINE LL_EXTI_LINE_22
+#	define PHASE_A_COMP_NUMBER COMP2
+
+
+#endif
+
 /************************************ G031 Hardware Groups
  * ************************************************/
 
@@ -2259,7 +2360,9 @@
 #	define INTERVAL_TIMER TIM2
 #	define TEN_KHZ_TIMER TIM6
 #	define UTILITY_TIMER TIM17
-#	define COM_TIMER TIM16
+#	ifndef COM_TIMER
+#		define COM_TIMER TIM16
+#	endif
 #	define APPLICATION_ADDRESS 0x08001000
 #	define MAIN_COMP COMP2
 #	define EXTI_LINE LL_EXTI_LINE_22
@@ -2281,9 +2384,25 @@
 #	ifndef AF_C_LOW
 #		define AF_C_LOW LL_GPIO_AF_6
 #	endif
+#	ifndef CAN_TX_PIN
+#		define CAN_TX_PIN LL_GPIO_PIN_12
+#		define CAN_TX_PORT GPIOA
+#	endif
+#	ifndef CAN_RX_PIN
+#		define CAN_RX_PIN LL_GPIO_PIN_11
+#		define CAN_RX_PORT GPIOA
+#	endif
+#	ifndef VOLTAGE_ADC_PORT
+#		define VOLTAGE_ADC_PORT GPIOA
+#	endif
+#	ifndef CURRENT_ADC_PORT
+#		define CURRENT_ADC_PORT GPIOA
+#	endif
 
 #	define DSHOT_PRIORITY_THRESHOLD 60
-#	define COM_TIMER_IRQ TIM1_UP_TIM16_IRQn
+#	ifndef COM_TIMER_IRQ
+#		define COM_TIMER_IRQ TIM1_UP_TIM16_IRQn
+#	endif
 #endif
 
 #ifdef MCU_GDE23