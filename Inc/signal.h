2838163ee6537965446a3869cb0aafad350a4be5
Jacob Dahl <dahl.jakejacob@gmail.com>
2026-08-15 15:29:57 -0600

    refactor(signal): drop duplicate externs in signal.h
    inputSet, dshot, and servoPwm were declared twice in the same header.

--- Inc/signal.h
+++ Inc/signal.h
@@ -19,8 +19,5 @@ extern uint16_t servo_low_threshold;  // anything below this point considered 0
 extern uint16_t servo_high_threshold; // anything above this point considered 2000 (max)
 extern uint16_t servo_neutral;
 extern uint8_t servo_dead_band;
-extern volatile char inputSet;
-extern char dshot;
-extern volatile char servoPwm;
 
 void detectInput();
